a5eefea6f2d26fbcd347de8018779401b8d4b615
Matt Muszynski <matt.muszynski@jpl.nasa.gov>
2026-05-05 12:41:17 -0700

    more traces

--- src/tts_tower/tower.py
+++ src/tts_tower/tower.py
@@ -164,9 +164,14 @@ class Tower(ABC):
         for rr in rule_results: 
             if rr['Criticality'] == 'DELETED': rr['Status'] = 'DELETED'
             if rr['Criticality'] == 'I': rr['Status'] = 'INFO_ONLY'
+
+        import pdb; pdb.set_trace()
         rule_results = rule_results.sort(by='Rule ID')
+        import pdb; pdb.set_trace()
         rule_results = rule_results.sort(lam=lambda x: status_order.get(x['Status'].upper(), -1), reverse=True)
+        import pdb; pdb.set_trace()
         rule_results = rule_results.sort(lam=lambda x: maturity_order.get(x['Maturity'].upper(), -1), reverse=True)
+        import pdb; pdb.set_trace()
 
 
         #hack to get us over the hump and put DELETED at the end without having to change upstream code
