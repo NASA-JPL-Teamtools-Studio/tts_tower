19c94537bec952cddb61a39cb9b08a2068fd70ce
Matt Muszynski <matt.muszynski@jpl.nasa.gov>
2026-05-05 11:48:04 -0700

    flips sorting of status and maturity

--- src/tts_tower/tower.py
+++ src/tts_tower/tower.py
@@ -165,8 +165,8 @@ class Tower(ABC):
             if rr['Criticality'] == 'DELETED': rr['Status'] = 'DELETED'
             if rr['Criticality'] == 'I': rr['Status'] = 'INFO_ONLY'
         rule_results = rule_results.sort(by='Rule ID')
-        rule_results = rule_results.sort(lam=lambda x: maturity_order.get(x['Maturity'].upper(), -1), reverse=True)
         rule_results = rule_results.sort(lam=lambda x: status_order.get(x['Status'].upper(), -1), reverse=True)
+        rule_results = rule_results.sort(lam=lambda x: maturity_order.get(x['Maturity'].upper(), -1), reverse=True)
 
 
         #hack to get us over the hump and put DELETED at the end without having to change upstream code
